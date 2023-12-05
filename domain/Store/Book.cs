85eb3a3fff07c6236f0b4581122abed71e0d7043
MaksimKrasnov <krasnovm020@gmail.com>
2023-12-05 16:01:50 +0700

    Добавлен класс книга

---
+++ domain/Store/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Store;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string? Title { get;}
+
+    public Book (int id,string title)
+    {
+        Title= title;
+        Id = id;
+    }
+
+}
